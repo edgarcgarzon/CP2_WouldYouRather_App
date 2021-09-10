b744f2994cf78d172b1f7f3b24e94b556953b900
Edgar <Garzon>
2021-09-10 22:52:44 +0200

    [feat] add logic for home navigation

--- src/Components/QuestionBoard.js
+++ src/Components/QuestionBoard.js
@@ -4,67 +4,82 @@ import { withRouter } from "react-router-dom";
 import compose from 'recompose/compose'
 import QuestionBoardItem from './QuestionBoardItem'
 import {
-    Tabs,
-    Tab,
-    Grid,
+  Tabs,
+  Tab,
+  Grid,
 } from '@material-ui/core';
 
 export class QuestionBoard extends Component {
 
-    constructor(props){
-        super(props)
-        this.state = {tab: "Unanswered Questions"}
-    }
+  constructor(props) {
+    super(props)
+    this.state = { tab: "Unanswered Questions" }
+  }
 
-    viewPull = (id) => {
-        this.props.history.push(`/questions/${id}`)
-    }
+  viewPull = (id) => {
+    this.props.history.push(`/questions/${id}`)
+  }
 
-    tabChangeHandler = (e, newValue) =>{
-        this.setState({tab:newValue})
-    }
+  tabChangeHandler = (e, newValue) => {
+    this.setState({ tab: newValue })
+  }
 
-    render() {
+  questionAnswered(question, authedUser) {
+    return question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser)
+  }
 
-        const questions = Object.values(this.props.questions)
-        return (
-            <div>
-                <Tabs
-                    value={this.state.tab}
-                    disabled={true}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    centered
-                    style={{marginBottom:20}}
-                    onChange={this.tabChangeHandler}
-                >
-                    <Tab label="Unanswered Questions" value={"Unanswered Questions"}/>
-                    <Tab label="Answered Questions" value={"Answered Questions"}/>
-                </Tabs>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    {questions.map((question) => (
-                        <Grid item key={question.id+1}>
-                            <QuestionBoardItem key={question.id} question={question} user={this.props.users[question.author]} viewPull={this.viewPull} />
-                        </Grid>
-                    ))}
+  display
 
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+
+    const questions = Object.values(this.props.questions)
+
+    return (
+      <div>
+        <Tabs
+          value={this.state.tab}
+          disabled={true}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+          style={{ marginBottom: 20 }}
+          onChange={this.tabChangeHandler}
+        >
+          <Tab label="Unanswered Questions" value={"Unanswered Questions"} />
+          <Tab label="Answered Questions" value={"Answered Questions"} />
+        </Tabs>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {questions.map((question) => {
+
+            const displayQuestion = this.state.tab == "Answered Questions"
+              ? this.questionAnswered(question, this.props.authedUser)
+              : !this.questionAnswered(question, this.props.authedUser)
+            return (
+              displayQuestion && (
+                <Grid item key={question.id + 1}>
+                  <QuestionBoardItem key={question.id} question={question} user={this.props.users[question.author]} viewPull={this.viewPull} />
+                </Grid>)
+            )
+          })}
+
+        </Grid>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = ({ users, questions }) => ({
-    users,
-    questions
+const mapStateToProps = ({ users, questions, authedUser }) => ({
+  users,
+  questions,
+  authedUser
 })
 
 export default compose(
-    connect(mapStateToProps)
+  connect(mapStateToProps)
 )(withRouter(QuestionBoard))
