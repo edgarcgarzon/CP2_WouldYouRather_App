5c9e318e9df1d65aca4531bc6aab3ba5ed346c26
Edgar <Garzon>
2021-09-13 08:20:08 +0200

    [chore] fix build problems

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import './index.css';
-import App from './components/App';
+import App from './Components/App';
 import reducers from './reducers';
 import middleware from './middleware';
 
